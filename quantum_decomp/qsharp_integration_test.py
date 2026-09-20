880567585511d2a4fa2fc1e0f75364319308fb76
Dmytro Fedoriaka <fedimser@gmail.com>
2026-09-20 13:59:12 -0700

    fix q# tests

--- quantum_decomp/qsharp_integration_test.py
+++ quantum_decomp/qsharp_integration_test.py
@@ -4,12 +4,11 @@ an operation, which applies specified matrix to a register of qubits.
 """
 
 import io
-import tempfile
 from contextlib import redirect_stdout
 
 import cirq
 import numpy as np
-import qsharp
+from qdk import qsharp
 from scipy.stats import unitary_group
 
 import quantum_decomp as qd
@@ -36,28 +35,22 @@ def dump_qsharp_unitary(op_code, qubits_count):
     return:
         np.array - unitary matrix implemented by given operation.
     """
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        with open(tmp_dir + "/qsharp.json", "w") as f:
-            f.write('{"files":["Op.qs"]}')
-        with open(tmp_dir + "/Op.qs", "w") as f:
-            f.write(op_code)
-        qsharp.init(project_root=tmp_dir,
-                    target_profile=qsharp.TargetProfile.Base)
-        f = io.StringIO()
-        with redirect_stdout(f):
-            qsharp.eval(
-                f'Std.Diagnostics.DumpOperation({qubits_count}, Op.Op);')
-        dump_output = f.getvalue()
+    qsharp.eval(op_code)
 
-    tokens = dump_output.replace('𝑖', 'j').replace('−', '-').split(' ')[1:]
+    f = io.StringIO()
+    with redirect_stdout(f):
+        qsharp.eval(f"Std.Diagnostics.DumpOperation({qubits_count}, Op);")
+    dump_output = f.getvalue()
+
+    tokens = dump_output.replace("𝑖", "j").replace("−", "-").split(" ")[1:]
     values = np.array([np.complex128(x) for x in tokens], dtype=np.complex128)
-    assert len(values == (2**qubits_count)**2)
+    assert len(values == (2**qubits_count) ** 2)
     ans = values.reshape((2**qubits_count, 2**qubits_count))
     return change_matrix_endianness(ans, qubits_count)
 
 
 def check_on_matrix(matrix):
-    op_code = qd.matrix_to_qsharp(matrix, op_name='Op')
+    op_code = qd.matrix_to_qsharp(matrix, op_name="Op")
     qubits_count = int(np.log2(matrix.shape[0]))
     dump_matrix = dump_qsharp_unitary(op_code, qubits_count)
     assert cirq.equal_up_to_global_phase(matrix, dump_matrix, atol=2e-4)
